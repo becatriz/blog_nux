888938dd7a5d1c6c5ca989543106d70c66507436
Rebeca Lopes <becatriz7@gmail.com>
2021-02-16 21:46:53 -0400

    feat: cria layouts

---
+++ layouts/error.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="error-page">
+    <h1>Oops, ocorreu um erro!</h1>
+    <p>Voltar para <a href="/">tire-me daqui</a>!</p>
+  </div>
+</template>
+
+<style scoped>
+.error-page {
+  text-align: center;
+}
+
+.error-page a {
+  text-decoration: none;
+  color: red;
+}
+
+.error-page a:hover,
+.error-page a:active {
+  color: salmon;
+}
+</style>
